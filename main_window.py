28d13667256fc39f75f1bcfeabd302ba45cbc57c
mostaelectron <mostaelectron@yahoo.fr>
2018-02-23 12:17:47 +0100

    first commit

---
+++ main_window.py
@@ -0,0 +1,102 @@
+"""
+In this example, we demonstrate how to create simple face detection using Opencv3 and PyQt5
+
+Author: Berrouba.A
+Last edited: 23 Feb 2018
+"""
+
+# import system module
+import sys
+
+# import some PyQt5 modules
+from PyQt5.QtWidgets import QApplication
+from PyQt5.QtWidgets import QWidget
+from PyQt5.QtGui import QImage
+from PyQt5.QtGui import QPixmap
+from PyQt5.QtCore import QTimer
+
+# import Opencv module
+import cv2
+
+from ui_main_window import *
+
+class MainWindow(QWidget):
+    # class constructor
+    def __init__(self):
+        # call QWidget constructor
+        super().__init__()
+        self.ui = Ui_Form()
+        self.ui.setupUi(self)
+
+        # load face cascade classifier
+        self.face_cascade = cv2.CascadeClassifier('haarcascade/haarcascade_frontalface_alt.xml')
+
+        # create a timer
+        self.timer = QTimer()
+        # set timer timeout callback function
+        self.timer.timeout.connect(self.detectFaces)
+
+        # set control_bt callback clicked  function
+        self.ui.control_bt.clicked.connect(self.controlTimer)
+
+    # detect face
+    def detectFaces(self):
+        # read frame from video capture
+        ret, frame = self.cap.read()
+
+        # resize frame image
+        scaling_factor = 0.8
+        frame = cv2.resize(frame, None, fx=scaling_factor, fy=scaling_factor, interpolation=cv2.INTER_AREA)
+
+        # convert frame to GRAY format
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+        # detect rect faces
+        face_rects = self.face_cascade.detectMultiScale(gray, 1.3, 5)
+
+        # for all detected faces
+        for (x, y, w, h) in face_rects:
+            # draw green rect on face
+            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+        # convert frame to RGB format
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+        # get frame infos
+        height, width, channel = frame.shape
+        step = channel * width
+        # create QImage from RGB frame
+        qImg = QImage(frame.data, width, height, step, QImage.Format_RGB888)
+        # show frame in img_label
+        self.ui.image_label.setPixmap(QPixmap.fromImage(qImg))
+
+
+
+    # start/stop timer
+    def controlTimer(self):
+        # if timer is stopped
+        if not self.timer.isActive():
+            # create video capture
+            self.cap = cv2.VideoCapture(0)
+            # start timer
+            self.timer.start(20)
+            # update control_bt text
+            self.ui.control_bt.setText("Stop")
+        # if timer is started
+        else:
+            # stop timer
+            self.timer.stop()
+            # release video capture
+            self.cap.release()
+            # update control_bt text
+            self.ui.control_bt.setText("Start")
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+
+    # create and show mainWindow
+    mainWindow = MainWindow()
+    mainWindow.show()
+
+    sys.exit(app.exec_())
---
+++ ui_main_window.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'ui_main_window.ui'
+#
+# Created by: PyQt5 UI code generator 5.5.1
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+class Ui_Form(object):
+    def setupUi(self, Form):
+        Form.setObjectName("Form")
+        Form.resize(556, 381)
+        self.verticalLayout = QtWidgets.QVBoxLayout(Form)
+        self.verticalLayout.setObjectName("verticalLayout")
+        self.image_label = QtWidgets.QLabel(Form)
+        self.image_label.setObjectName("image_label")
+        self.verticalLayout.addWidget(self.image_label)
+        self.control_bt = QtWidgets.QPushButton(Form)
+        self.control_bt.setObjectName("control_bt")
+        self.verticalLayout.addWidget(self.control_bt)
+
+        self.retranslateUi(Form)
+        QtCore.QMetaObject.connectSlotsByName(Form)
+
+    def retranslateUi(self, Form):
+        _translate = QtCore.QCoreApplication.translate
+        Form.setWindowTitle(_translate("Form", "Face detection"))
+        self.image_label.setText(_translate("Form", "TextLabel"))
+        self.control_bt.setText(_translate("Form", "Start"))
+
